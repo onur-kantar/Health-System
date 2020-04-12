a60d89b03d9dcdfcf373b2ab3ef4c1ec8011b259
Onur Kantar <onurkantr@gmail.com>
2020-04-13 02:38:38 +0300

    Design and mechanism are ready

---
+++ Health System/Assets/Codes/GameHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public HealthBar healthBar;
+    void Start()
+    {
+        HealthSystem healthSystem = new HealthSystem(100);
+        Debug.Log("Health : " + healthSystem.GetHealthPercent());
+        healthBar.Setup(healthSystem);
+        healthSystem.Damage(10);
+        Debug.Log("Health : " + healthSystem.GetHealthPercent());
+
+    }
+}
