9810d253394f613f2b4b247b836689f937d445ef
Onur Kantar <onurkantr@gmail.com>
2020-04-14 23:01:14 +0300

    movement

---
+++ Health System/Assets/Codes/Hero/HealthSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class HealthSystem
+{
+    private int health;
+    private int healthMax;
+    public event EventHandler OnHealthChanged;
+    public HealthSystem(int healthMax)
+    {
+        this.healthMax = healthMax;
+        this.health = healthMax;
+    }
+    public void Damage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health < 0)
+            health = 0;
+        OnHealthChanged(this, EventArgs.Empty);
+
+    }
+    public void Heal(int healAmount)
+    {
+        health += healAmount;
+        if (health > healthMax)
+            health = healthMax;
+        OnHealthChanged(this, EventArgs.Empty);
+    }
+    public float GetHealthPercent()
+    {
+        return (float)health / healthMax;
+    }
+    public int GetHealth()
+    {
+        return health;
+    }
+
+}
---
+++ Health System/Assets/Codes/Hero/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    void Awake()
+    {
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(Input.GetAxisRaw("Vertical")+"," +Input.GetAxisRaw("Horizontal") + " "+ Time.fixedDeltaTime);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
